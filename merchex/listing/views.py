4197aa9a6561cf3da4118c0de24d627a3b74808a
Wilfried Dang <wilfrieddang@yahoo.com>
2022-02-23 19:26:16 +0100

    Premiere page web avec titre h1

--- merchex/listing/views.py
+++ merchex/listing/views.py
@@ -1,3 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Bienvenu sur mon app Django</h1>")
