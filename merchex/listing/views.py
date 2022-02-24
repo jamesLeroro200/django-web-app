bc99693e32e0f2fe52e6b15241e8103a7cbb9a88
Wilfried Dang <wilfrieddang@yahoo.com>
2022-02-24 04:09:52 +0100

    ajout des gabaris django hello.html contactus.html about.html listings.html

--- merchex/listing/views.py
+++ merchex/listing/views.py
@@ -6,21 +6,14 @@ from listing.models import Listings
 
 def hello(request):
     bands = Band.objects.all()
-    listings= Listings.objects.all()
-    return HttpResponse(f"""
-    <h1>Bienvenu sur mon app Django</h1>
-    <p>Mes meilleurs groupes sont :</p>
-        <ul>
-            <li>{bands[0].name} <p>{listings[0].titre}</p></li>
-            <li>{bands[1].name} <p>{listings[1].titre}</p></li>
-            <li>{bands[2].name} <p>{listings[2].titre}</p></li>
-        <ul/>
-    """)
+    return render(request, "listings/hello.html", {'bands':bands, 'listings':listings})
 
 def apropo(request):
-    return HttpResponse("<h1>A propos </h1> <p>Nous adorons merch</p>")
+    return render(request, "listings/about.html")
 
 def listings(request):
-    return HttpResponse('/listing/views.py')
+    listings= Listings.objects.all()
+    return render(request, "listings/listings.html", {'listings':listings})
+    
 def contactus(request):
-    return HttpResponse('/listing/contactus.py')
+    return render(request, "listings/contactus.html")
